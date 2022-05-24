93a1e06d9617c73d9a66d22a6a30a1fe68eb9af4
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2022-05-24 23:23:29 +0200

    added new methods to JwtUtils interface

--- src/main/java/com/janjakubowski/common/auth/util/JwtUtils.java
+++ src/main/java/com/janjakubowski/common/auth/util/JwtUtils.java
@@ -1,5 +1,7 @@
 package com.janjakubowski.common.auth.util;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.janjakubowski.common.auth.model.Principal;
 
 public interface JwtUtils {
@@ -9,8 +11,42 @@ public interface JwtUtils {
      * @param token String containing JWT token
      * @return Principal authentication principal
      * @see Principal
+     * @throws JWTDecodeException
      */
-    Principal decodeToken(String token);
+    Principal decodeToken(String token) throws JWTDecodeException;
+
+    /**
+     * Method decode JWT token if valid
+     * 
+     * @param token String containing JWT token
+     * @return Decoded JWT
+     * @see Principal
+     * @throws JWTDecodeException
+     */
+    DecodedJWT decodeTokenToRaw(String token) throws JWTDecodeException;
+
+    /**
+     * Method to decode Mobile JWT token if valid
+     * For some cases it can differ from standard access token,
+     * so separate method allows more flexibility
+     * 
+     * @param token
+     * @return authentication principal
+     * @throws JWTDecodeException
+     */
+    Principal decodeMobileToken(String token) throws JWTDecodeException;
+
+    /**
+     * Method to decode Mobile JWT token if valid
+     * For some cases it can differ from standard access token,
+     * so separate method allows more flexibility
+     * 
+     * @param token
+     * @return Decoded JWT
+     * @see DecodedJWT
+     * @throws JWTDecodeException
+     */
+    DecodedJWT decodeMobileTokenToRaw(String token) throws JWTDecodeException;
 
     /**
      * @param principal - subject to generate token for
