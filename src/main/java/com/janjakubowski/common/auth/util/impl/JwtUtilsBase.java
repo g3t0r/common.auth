64bb9c0ebe12708548ffef8a3bbb6b62175a8116
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2022-05-26 16:30:06 +0200

    removed unused commit

--- src/main/java/com/janjakubowski/common/auth/util/impl/JwtUtilsBase.java
+++ src/main/java/com/janjakubowski/common/auth/util/impl/JwtUtilsBase.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.janjakubowski.common.auth.model.JwtConfig;
-import com.janjakubowski.common.auth.model.Principal;
 import com.janjakubowski.common.auth.util.JwtUtils;
 
 import lombok.RequiredArgsConstructor;
